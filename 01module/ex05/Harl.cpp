fed7db75f4501d7e9a8bef88ff5279d6ad7ab432
Verinha <pedro.coimbra.vera@gmail.com>
2023-03-23 11:07:54 +0000

    start of ex05

---
+++ 01module/ex05/Harl.cpp
@@ -0,0 +1,38 @@
+#include "Harl.hpp"
+
+Harl::Harl()
+{
+	std::cout << "MA BOY HARL IS BACK!" << std::endl;
+	return ;
+}
+
+Harl::~Harl()
+{
+	std::cout << "Awww, harl is gone..." << std::endl;
+	return ;
+}
+
+void	Harl::debug(void)
+{
+	std::cout << "love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!" << std::endl;
+}
+
+void	Harl::info(void)
+{
+	std::cout << "cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!" << std::endl;
+}
+
+void	Harl::warning(void)
+{
+	std::cout << "I think I deserve to have some extra bacon for free. I've been coming for years whereas you started working here since last month." << std::endl;
+}
+
+void	Harl::error(void)
+{
+	std::cout << "his is unacceptable! I want to speak to the manager now." << std::endl;
+}
+
+void	Harl::complain(std::string level)
+{
+	
+}
