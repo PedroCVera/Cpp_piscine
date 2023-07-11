cbb3fcea123b4d308d46ebf1a0ee44caeb510c26
Pedro Coimbra vera <pcoimbra@c2r7s2.42lisboa.com>
2023-07-11 14:10:33 +0100

    module 5 ex00 done and start 01

---
+++ 05module/ex01/Form.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Form.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pcoimbra <pcoimbra@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/11 14:02:48 by pcoimbra          #+#    #+#             */
+/*   Updated: 2023/07/11 14:05:51 by pcoimbra         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FORM_HPP
+# define FORM_HPP
+
+# include <iostream>
+# include <string>
+# include <exception>
+
+class Form
+{
+	private:
+		const std::string	_fname;
+		int					_sign_grade;
+		int					_exec_grade;
+		bool				_sign;
+
+	public:
+	
+};
+
+#endif
