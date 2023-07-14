3cc426d2b4448e27d92580211f855c440c7b0428
Pedro Coimbra vera <pcoimbra@c2r10s7.42lisboa.com>
2023-07-14 16:33:06 +0100

    forms almost done

--- 05module/ex01/Form.cpp
+++ 05module/ex01/Form.cpp
@@ -6,10 +6,47 @@
 /*   By: pcoimbra <pcoimbra@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/11 14:02:32 by pcoimbra          #+#    #+#             */
-/*   Updated: 2023/07/11 14:03:39 by pcoimbra         ###   ########.fr       */
+/*   Updated: 2023/07/14 16:31:51 by pcoimbra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Form.hpp"
 
 
+Form::Form()
+{
+	std::cout << "Default Form has been created!" << std::endl;
+	_exec_grade = 1;
+	_sign = false;
+	_sign_grade = 1;
+}
+
+Form::Form(Form const & copy) : _fname(copy._fname) , _sign_grade(copy._sign_grade) , _exec_grade(copy._exec_grade) , _sign(copy._sign)
+{
+	std::cout << "form has been copied!" << _fname << std::endl;
+	return ;
+}
+
+Form::~Form(void)
+{
+	std::cout << "Form " << _fname << " has been destroyed." << std::endl;
+	return ;
+}
+
+Form &Form::operator=(Form const &other)
+{
+	_grade = other.getGrade();
+	return (*this);
+}
+
+Form::Form(const std::string name, int sign_grade, int exec_grade) : _fname(name)
+{
+	if (sign_grade < 1 || exec_grade < 1)
+		throw Form::GradeTooHighException();
+	if (sign_grade > 150 || exec_grade > 150)
+		throw Form::GradeTooLowException();
+	_sign_grade = sign_grade;
+	_exec_grade = exec_grade;
+	std::cout << "Constructor with input " << _fname << "." << std::endl;
+	return ;
+}
--- 05module/ex01/Form.hpp
+++ 05module/ex01/Form.hpp
@@ -6,7 +6,7 @@
 /*   By: pcoimbra <pcoimbra@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/11 14:02:48 by pcoimbra          #+#    #+#             */
-/*   Updated: 2023/07/11 14:05:51 by pcoimbra         ###   ########.fr       */
+/*   Updated: 2023/07/14 16:22:36 by pcoimbra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,33 @@ class Form
 		bool				_sign;
 
 	public:
-	
+		Form();
+		Form(Form const & copy);
+		Form &operator=(Form const &other);
+		~Form();
+
+		Form(const std::string name, int sign_grade, int exec_grade);
+
+		std::string getName() const;
+		int			getSignGrade() const;
+		int			getExecGrade() const;
+		bool		getSign() const;
+
+		void		beSigned(Bureaucrat const &Bur);
+		
+		class	GradeTooHighException : std::exception
+		{
+			public:
+				const char *what() const throw();
+		};
+		class	GradeTooLowException : std::exception
+		{
+			public:
+				const char *what() const throw();
+		};
+
 };
+
+std::ostream &operator<<(std::ostream &out, const Bureaucrat &b);
 
 #endif
